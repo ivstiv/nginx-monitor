f946831a9c5d0fef4f046c2a02b3dd8c7234f094
Ivan <ivan21039@gmail.com>
2020-06-22 22:02:46 +0100

    Adding the script

---
+++ README.md
@@ -0,0 +1,47 @@
+# nginx-monitor
+
+To automatically monitor an NGINX server and activate CloudFlare firewall rules according to changes in the rate of active connections. To set it up you will need an API key that has permission to the said cloudflare zone. Edit the first few lines of the script to suit your setup and traffic. 
+
+## Requirements and dependencies
+
+- A system running GNU/Linux
+- NGINX (obviously :D)
+- curl
+- jq
+- awk
+
+## Setup
+
+Make sure that your NGINX has the [http_stub_status_module](https://nginx.org/en/docs/http/ngx_http_stub_status_module.html) which is usually enabled by default. You can check whether the modules is enabled via `nginx -V | grep --color -o http_stub_status`. Once you have that enabled you will need to add this server block to your NGINX config. 
+If done on CentOS just create a file `/etc/nginx/conf.d/monitor.conf`:
+```
+server {
+    listen 127.0.0.1:80;
+    location /nginx_status {
+        stub_status on;
+        access_log off;
+        allow 127.0.0.1;
+        deny all;
+    }
+}
+```
+Reload the config with `nginx -t && nginx -s reload` to apply the new server block. Try to get information from the new server block to see if everything is working `curl -s 127.0.0.1/nginx_status`. This should show information about current connections to the web server. 
+
+Setup your API key and zone ID, adjust the variables according to the amount of traffic that your server gets and just run the script to see if it is working: `sh nginx-monitor.sh`. **If you want to disable logging just leave the variable $LOG empty!**
+
+## Examples
+Since the script takes the running shell it would be a good idea to run it in a detached tmux or screen session. Here some exampels of how you can achieve that. 
+
+**To run a detached screen session with the script:**`screen -dmS nginx-monitor sh nginx-monitor.sh`
+**Adding the script to run atomatically on reboot:**`@reboot screen -dmS nginx-monitor sh nginx-monitor.sh`
+**To attach to the script's session:**`screen -r nginx-monitor`
+**To detach from the session:**`CTRL+A+D`
+**To kill the session:**`CTRL+A+K`
+
+# Links and additional info
+I don't see how this can be further developed but if you have any ideas you are welcome to [join my discord](https://discord.gg/VMSDGVD) and ask for help or give me a heads up for problems with the script.
+
+
+
+
+
---
+++ nginx-monitor.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+
+# How frequently the script checks the active connections
+CHECK_RATE=10
+# How big of a jump should the script tolerate between 2 checks
+DIFF_THRESHOLD=70
+# How many active connections should the script tolerate before activating the firewall rule
+TOTAL_THRESHOLD=900
+# Log the output to file
+project_root=$(dirname "$(realpath "$0")")
+LOG="$project_root/monitor.log"
+
+# Cloudflare needed variables
+API_KEY=""
+ZONE_ID=""
+RULE_DESC=""
+
+# Turns on a rule based on its description
+turnRuleOn() {
+    newBody=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/firewall/rules" \
+        -H "Authorization: Bearer $API_KEY" \
+        -H "Content-Type:application/json" | jq ".result[] | select(.description == \"$RULE_DESC\") | .paused = false")
+
+    response=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/firewall/rules" \
+        -H "Authorization: Bearer $API_KEY" \
+        -H "Content-Type: application/json" \
+        --data "[$newBody]" | jq '.success')
+    echo "    - Success: $response"
+    [ -n "$LOG" ] && echo "    - Success: $response" >> "$LOG"
+}
+
+# Turns off a rule based on its description
+turnRuleOff() {
+    newBody=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/firewall/rules" \
+        -H "Authorization: Bearer $API_KEY" \
+        -H "Content-Type:application/json" | jq ".result[] | select(.description == \"$RULE_DESC\") | .paused = true")
+
+    response=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/firewall/rules" \
+        -H "Authorization: Bearer $API_KEY" \
+        -H "Content-Type: application/json" \
+        --data "[$newBody]" | jq '.success')
+    echo "    - Success: $response"
+    [ -n "$LOG" ] && echo "    - Success: $response" >> "$LOG"
+}
+
+checkDependencies() {
+    mainShellPID="$$"
+    printf "curl\njq\nawk" | while IFS= read -r program; do
+        if ! [ -x "$(command -v "$program")" ]; then
+            echo "Error: $program is not installed." >&2
+            kill -9 "$mainShellPID" 
+        fi
+    done
+}
+
+checkDependencies
+
+
+# If zone id or rule id are empty exit
+if [ -z "$ZONE_ID" ] || [ -z "$RULE_DESC" ] ||  [ -z "$API_KEY" ]; then
+    echo "[Error]: ZONE_ID, RULE_DESC or API_KEY is empty!" >&2 
+    [ -n "$LOG" ] && echo "[Error]: ZONE_ID, RULE_DESC or API_KEY is empty!" >> "$LOG"
+    exit 1
+fi
+
+# Check if the api key is valid
+isKeyValid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" \
+    -H "Authorization: Bearer $API_KEY" \
+    -H "Content-Type:application/json" | jq '.result.status' --raw-output)
+
+if [ ! "$isKeyValid" = "active" ]; then
+    echo "[Error]: API key is invalid!" >&2
+    [ -n "$LOG" ] && echo "[Error]: API key is invalid!" >> "$LOG"
+    exit 1
+fi
+
+
+isRulePaused=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/firewall/rules" \
+     -H "Authorization: Bearer $API_KEY" \
+     -H "Content-Type:application/json" | jq ".result[] | select(.description == \"$RULE_DESC\") | .paused" --raw-output)
+
+prevConnections=0 currConnections=0 
+while true; do
+    currConnections=$(curl -s 127.0.0.1/nginx_status | awk 'NR==1{print $3; exit}')
+    timestamp=$(date +"%F %T")
+    echo "[$timestamp] Active connections: $currConnections"
+    [ -n "$LOG" ] && echo "[$timestamp] Active connections: $currConnections" >> "$LOG"
+    echo "    - Rule $RULE_DESC paused: $isRulePaused"
+    [ -n "$LOG" ] && echo "    - Rule $RULE_DESC paused: $isRulePaused" >> "$LOG"
+
+    # check difference between previous and current active connections
+    if [ $((currConnections - prevConnections)) -gt $DIFF_THRESHOLD ]; then
+        echo "    - DIFF_THRESHOLD ($DIFF_THRESHOLD) exceeded!"
+        [ -n "$LOG" ] && echo "    - DIFF_THRESHOLD ($DIFF_THRESHOLD) exceeded!" >> "$LOG"
+        # check if the rule is enabled if not enable it
+        if [ "$isRulePaused" = "true" ]; then
+            echo "    - Turning on the rule"
+            [ -n "$LOG" ] && echo "    - Turning on the rule" >> "$LOG"
+            turnRuleOn
+            isRulePaused=false
+        fi
+
+    elif [ "$currConnections" -gt $TOTAL_THRESHOLD ];then
+        echo "    - TOTAL_THRESHOLD ($TOTAL_THRESHOLD) exceeded!"
+        [ -n "$LOG" ] && echo "    - TOTAL_THRESHOLD ($TOTAL_THRESHOLD) exceeded!" >> "$LOG"
+        # check if the rule is enabled if not enable it
+        if [ "$isRulePaused" = "true" ]; then
+            echo "    - Turning on the rule"
+            [ -n "$LOG" ] && echo "    - Turning on the rule" >> "$LOG"
+            turnRuleOn
+            isRulePaused=false
+        fi
+
+    else
+        # if the rule is on turn it off
+        if [ "$isRulePaused" = "false" ]; then
+            echo "    - Turning off the rule, traffic is under the treshold."
+            [ -n "$LOG" ] && echo "    - Turning off the rule, traffic is under the treshold." >> "$LOG"
+            turnRuleOff
+            isRulePaused=true
+        fi
+    fi
+
+    prevConnections=currConnections
+    sleep "$CHECK_RATE"
+done
